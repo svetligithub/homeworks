7d9f065db4190b833f21341a648dfc7f49137c64
svetligithub <pop3angelov@abv.bg>
2015-06-29 18:49:02 +0300

    Create zad3.php

---
+++ 16.06/zad3.php
@@ -0,0 +1,11 @@
+<?php
+$integer = 37;
+$float = 37.6;
+$boolean = false;
+$string = "This is a string";
+echo 'The main variable types are:' . '<br>';
+echo gettype($integer). '<br>';
+echo gettype($float). '<br>';
+echo gettype($boolean). '<br>';
+echo gettype($string);
+?>
