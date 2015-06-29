a2113de26b2f50ea50521198000c76f8e961db88
svetligithub <pop3angelov@abv.bg>
2015-06-29 18:50:42 +0300

    Create zad6.php

---
+++ 16.06/zad6.php
@@ -0,0 +1,4 @@
+<?php
+$movies = array('Terminator' => 'Arnold Schwarzenegger', 'Alien' => 'Sigourney Weaver', 'Fast & Furious' => 'Vin Diesel');
+echo $movies['Terminator'] . '<br>' . $movies['Alien'];
+?>
