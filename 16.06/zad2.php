77422748e29c35a387c4ea7c243d04b120dc3335
svetligithub <pop3angelov@abv.bg>
2015-06-29 18:48:13 +0300

    Create zad2.php

---
+++ 16.06/zad2.php
@@ -0,0 +1,8 @@
+<?php
+$side = 25;
+$height = 15;
+$surface = $side * $height;
+$perimeter = ($side + $height) * 2;
+echo 'The surface of the rectangle is ' . "$surface" . '<br>';
+echo 'The perimeter of the rectangle is ' . "$perimeter";
+?>
